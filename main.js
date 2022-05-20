9a9701915795e05a05329f2564f5b362e683ccb8
erikia <nobileisabelle.in@gmail.com>
2022-05-20 20:22:01 +0200

    waiting for review

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const TOP_RANKED = "http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&pag
 const BEST_ALL_CATEGORIES = "http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&page_size=7&page=1";
 const BEST_ACTION = "http://localhost:8000/api/v1/titles/?genre=Action&sort_by=-imdb_score&page_size=7&page=1";
 const BEST_ADVENTURE = "http://localhost:8000/api/v1/titles/?genre=Adventure&sort_by=-imdb_score&page_size=7&page=1";
-
+const movie = document.getElementById('movie');
 
 class Movies {
     constructor(url, index) {
@@ -22,6 +22,29 @@ class Movies {
         } else {
             console.error('Retour du serveur: ', response.status)
         }
+    }
+
+    showMovies(data) {
+        movie.innerHTML = '';
+
+        data.forEach(movie => {
+            const { title, image_url, votes, id } = movie;
+            const movieEl = document.createElement('div');
+            movieEl.classList.add('movie');
+            movieEl.innerHTML = `
+                <img src="${image_url}" alt="${title}">
+                <div class="movie-info">
+                    <h2>${title}</h2>
+                    <span class="${(votes)}">${votes}</span>
+                </div>`
+
+            main.appendChild(movieEl);
+
+            document.getElementById(id).addEventListener('click', () => {
+                console.log(id)
+                openNav(movie)
+            })
+        })
     }
 
     // run () {
